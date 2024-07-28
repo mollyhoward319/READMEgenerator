4570aa282d00294744a4352f472f5a2f8b8722bf
Molly Howard <mollyhoward319@gmail.com>
2024-07-27 20:16:14 -0500

    added starter code

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    message: 'What was your motivation?',
+    name: 'motivation',
+}
+
+{
+    type: 'input',
+    message: 'Why did you build this project?',
+    name: 'motivation',
+    }
+
+    {
+        type: 'input',
+        message: 'What problem does it solve?',
+        name: 'motivation',
+        }
+
+        {
+            type: 'input',
+            message: 'What makes your project stand out?',
+            name: 'motivation',
+            }
+        ]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
